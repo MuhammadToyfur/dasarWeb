8eba65494a6922584939bcb1c02d9a4343d9f184
MuhammadToyfur <muhammadtoyfurditaramadan@gmail.com>
2024-03-04 11:03:27 +0700

    Pertemuan 4 - Praktikum 3 (3.6)

---
+++ PHP/kursi.php
@@ -0,0 +1,9 @@
+<?php
+$total_kursi = 45;
+$kursi_terisi = 28;
+$kursi_kosong = $total_kursi - $kursi_terisi;
+$persentase_kosong = ($kursi_kosong / $total_kursi) * 100;
+
+echo "Jumlah kursi yang masih kosong: $kursi_kosong <br>";
+echo "Persentase kursi yang masih kosong: $persentase_kosong %";
+?>
