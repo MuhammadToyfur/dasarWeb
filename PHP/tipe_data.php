7411b5ab448a9d3cf014a6148a9b367486794cf0
MuhammadToyfur <muhammadtoyfurditaramadan@gmail.com>
2024-03-04 09:35:21 +0700

    Pertemuan 4 - Praktikum 2

---
+++ PHP/tipe_data.php
@@ -0,0 +1,46 @@
+<?php
+$a = 10;
+$b = 5;
+$c = $a+ 5;
+$d = $b + (10*5);
+$e = $d +$c;
+
+echo "Variabel a :{$a} <br>";
+echo "Variabel b : {$b} <br>";
+echo "Variabel c : {$c} <br>";
+echo "Variabel d: {$d} <br>";
+echo "Variabel e : {$e} <br>";
+
+var_dump($e);
+ $nilaiMatematika = 5.1;
+ $nilaiIpa = 6.7;
+ $nilaiBahasaIndonesia = 9.3;
+
+ $rataRata = ($nilaiMatematika + $nilaiIpa + $nilaiBahasaIndonesia) / 3;
+
+ echo"Matematika: {$nilaiMatematika} <br>";
+ echo "IPA : {$nilaiIpa} <br>";
+ echo "Bahasa Indonesia {$nilaiBahasaIndonesia} <br>";
+ echo "Rata-rata: {$rataRata} <br>";
+ 
+ var_dump($rataRata);
+ 
+ $apakahSiswaLulus = true;
+ $apakahSiswaSudahUjian = false;
+
+ var_dump($apakahSiswaLulus);
+ echo "<br>";
+ var_dump($apakahSiswaSudahUjian);
+
+ $namaDepan = "Ibnu";
+ $namaBelakang = "Jakarta";
+
+ $namaLengkap = "{$namaDepan} {$namaBelakang}";
+ $namaLengkap2 = $namaDepan. ''. $namaBelakang;
+
+ echo "Nama Depan : {$namaDepan} <br>";
+ echo 'Nama Belakang: ' . $namaBelakang . '<br>';
+
+ $listMahasiswa = ["Wahid Badullah", "Elmo Bachtiar", "LEndis Fabri"];
+ echo $listMahasiswa[0];
+ ?>
