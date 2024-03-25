86015014f7a8b1a297537dc52caee68ad3292e6b
MuhammadToyfur <muhammadtoyfurditaramadan@gmail.com>
2024-03-25 11:22:16 +0700

    Pertemuan 5 - Praktikum1

---
+++ praktik_jquery/praktikum_jquery.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head> 
+        <title>Praktikum jQuery</title>
+        <script src="jquery-3.1.7.js"></script>
+    </head>
+    <body>
+        
+    </body>
+</html>
